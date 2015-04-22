c78fc404ef683f4a23265b60e3a558a574ff5afc
Herbert Spencer <herbert.spencer@gmail.com>
2015-04-22 00:24:16 -0300

    se remueve websocket
    porque ya no es necesario si no se trabaja con Processing

--- js/topologoscopio.js
+++ js/topologoscopio.js
@@ -1,61 +1,46 @@
-// Necesitamos asegurarnos que el navegador utilice Websocket
 
-        if ("WebSocket" in window) {
+// Ahora podemos comenzar el reconocimiento de voz
+// Sólo soportado por Google Chrome
+// Una vez que comienza, debes autorizar el acceso al micrófono
 
-            // Antes de conectarnos al Websocket, debemos comenzarlo desde Processing
+var recognition = new webkitSpeechRecognition();
 
-            var ws = new WebSocket("ws://localhost:8080/p5websocket");
-        } else {
+// De forma predeterminada, Google retorna solo 1 resultado
+// La forma continua mantiene el micrófono abierto
 
-            // El navegador no sporta websocket
+recognition.continuous = true;
+recognition.interimResults = true;
 
-            alert("WebSocket NOT supported by your Browser!");
-        }
+recognition.onresult = function(event) {
 
-        // Ahora podemos comenzar el reconocimiento de voz
-        // Sólo soportado por Google Chrome
-        // Una vez que comienza, debes autorizar el acceso al micrófono
+    // Obtén el resultado a partir del objeto generado por la API
+    var transcript = event.results[event.results.length-1][0].transcript;
+    var isFinal = event.results[event.results.length-1].isFinal;
+    var x;
+    
+    if(isFinal){
+    var d = document.getElementById("speech");
+        d.textContent += " · "+transcript;
+        d = document.getElementById("intermediate");
+        d.textContent = " ";
+        x = "true";
+    }else{
+    var d = document.getElementById("intermediate");
+        d.textContent = transcript;
+        x = "false";
+    }
 
-        var recognition = new webkitSpeechRecognition();
+    var newTranscript = transcript + "+" + x;
+    // ws.send(newTranscript);
+}
 
-        // De forma predeterminada, Google retorna solo 1 resultado
-        // La forma continua mantiene el micrófono abierto
+recognition.onerror = function(event) { 
+    recognition.start();
+}
+recognition.onend = function(event){
+    recognition.start();
+}
 
-        recognition.continuous = true;
-        recognition.interimResults = true;
-
-        recognition.onresult = function(event) {
-
-            // Obtén el resultado a partir del objeto generado por la API
-            var transcript = event.results[event.results.length-1][0].transcript;
-
-            var isFinal = event.results[event.results.length-1].isFinal;
-            
-            var x;
-            
-            if(isFinal){
-            var d = document.getElementById("speech");
-                d.textContent += " · "+transcript;
-                d = document.getElementById("intermediate");
-                d.textContent = " ";
-                x = "true";
-            }else{
-            var d = document.getElementById("intermediate");
-                d.textContent = transcript;
-                x = "false";
-            }
-
-            var newTranscript = transcript + "+" + x;
-            ws.send(newTranscript);
-        }
-
-        recognition.onerror = function(event) { 
-            recognition.start();
-        }
-        recognition.onend = function(event){
-            recognition.start();
-        }
-
-        // Comienza a reconocer
-        // recognition.lang = 'ES';
-        recognition.start();
+// Comienza a reconocer
+// recognition.lang = 'ES';
+recognition.start();
